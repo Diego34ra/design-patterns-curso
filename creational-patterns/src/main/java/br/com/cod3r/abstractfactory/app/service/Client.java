b4811d1544a0bb5a7aa2a1f593ef9619fa502356
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2025-09-03 15:40:17 -0300

    feat: adicionando funcionalidades abstract

--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
@@ -1,8 +1,24 @@
 package br.com.cod3r.abstractfactory.app.service;
 
+import br.com.cod3r.abstractfactory.app.service.factory.RestAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.factory.ServicesAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
 public class Client {
 
 	public static void main(String[] args) {
+		ServicesAbstractFactory factory = new RestAbstractFactory();
+
+		UserService userService = factory.getUserService();
+
+		userService.save("Joao");
+		userService.delete(1);
+
+		CarService carService = factory.getCarService();
+
+		carService.save("Carro");
+		carService.update("Carro3");
 
 	}
 }
--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,26 @@
 package br.com.cod3r.abstractfactory.apple;
 
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneFactory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRulesAbsctractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
+
 public class Client {
 	
 	public static void main(String[] args) {
+		CountryRulesAbstractFactory rules = new BrazilianRulesAbsctractFactory();
+		IPhoneFactory genXFactory = new IPhoneXFactory(rules);
+		IPhoneFactory gen11Factory = new IPhone11Factory(rules);
+
+		System.out.println("### Ordering an iPhone X");
+		IPhone iphoneX = genXFactory.orderIPhone("standard");
+		System.out.println(iphoneX);
+
+		System.out.println("### Ordering an iPhone 11");
+		IPhone iphone11 = gen11Factory.orderIPhone("standard");
+		System.out.println(iphone11);
 
 	}
 }
